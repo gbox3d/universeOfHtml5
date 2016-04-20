815109af86493c51e5641352fe593f54f4281623
도플광어 <gbox3d@gmail.com>
2016-04-20 18:58:22 +0900

    moment 예제 추가

---
+++ js/exstream_js/luaparser/test.lua
@@ -0,0 +1,9 @@
+app_config={
+    station_cfg = nil,--{"","",9,2},
+    ap_cfg={4},
+    remote={ip ="192.168.9.2",port=5683},
+    local_info={port=1471},
+    baud=115200,autostart=false,safedelay=25,
+    --parser=string.char(0x81)
+    parser=0
+}
