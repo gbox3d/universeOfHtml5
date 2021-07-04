3b9897c7be8407e05f2f512878c47bc406d59d9c
gbox3d <gbox3d@gmail.com>
2021-07-04 16:23:04 +0900

    fabricsjs vue

---
+++ vuejs/fbricsjs-example/README.md
@@ -0,0 +1,31 @@
+# fbricsjs-example
+
+vue와 fabricsjs 를 연동시켜보는 예제입니다.  
+fabricsjs npm 을 사용하여 구현하였습니다.  
+mounted 이밴트 처리함수에서 캔버스를 초기화 해줍니다.  
+
+## Project setup
+```
+npm install
+```
+
+### Compiles and hot-reloads for development
+```
+npm run serve
+```
+
+### Compiles and minifies for production
+```
+npm run build
+```
+
+### Lints and fixes files
+```
+npm run lint
+```
+
+### Customize configuration
+See [Configuration Reference](https://cli.vuejs.org/config/).  
+
+### 참고자료
+vue에서 fabrics 셋업하기 [바로가기](https://stackoverflow.com/questions/42344356/how-can-i-set-up-fabric-js-in-vue)
---
+++ vuejs/fbricsjs-example/src/App.vue
@@ -0,0 +1,43 @@
+<template>
+  <!-- <img alt="Vue logo" src="./assets/logo.png"> -->
+  <!-- <HelloWorld msg="Welcome to Your Vue.js App"/> -->
+  <canvas ref="mainCanvas" class="border-1"> </canvas>
+</template>
+
+<script>
+import { fabric } from "fabric";
+// import HelloWorld from './components/HelloWorld.vue'
+
+export default {
+  name: "App",
+  components: {
+    // HelloWorld
+  },
+  mounted() {
+    const ref = this.$refs.mainCanvas;
+    const canvas = new fabric.Canvas(ref);
+    const rect = new fabric.Rect({
+      fill: "red",
+      width: 20,
+      height: 20,
+    });
+    canvas.add(rect);
+  },
+};
+</script>
+
+<style>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 10px;
+}
+
+.border-1 {
+  border: 1px solid;
+}
+
+</style>
