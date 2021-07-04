28b245a55119e470af815b32f8e8f3080a82a73f
gbox3d <gbox3d@gmail.com>
2021-07-04 15:54:58 +0900

    ...

--- vuejs/hello/src/App.vue
+++ vuejs/hello/src/App.vue
@@ -1,7 +1,17 @@
 <template>
+
   <h1>{{ msg1 }}</h1>
   <input type="text" v-model="msg1">
-  <button @click="testClick" > test </button>
+  <hr>
+
+  <button @click="testClick" > test </button> <!-- () 가 생략되면 test($event) 와 같다. -->
+  <button @click="testClick()" > test call () </button>
+  <button @click="test2Click('hello')"> test2 </button>
+
+  <hr>
+  <p>{{ count }}</p>
+  <button @click="incCount(1,$event)">count 1</button>
+  <button @click="incCount(-1,$event)">count -1</button>
   
 </template>
 
@@ -12,12 +22,23 @@ export default {
   name: "App",
   data() {
     return {
+      count : 0,
       msg1: "Hello World",
     };
-  },
-  method : {
-    testClick() {
+  }
+  ,methods : {
+    testClick(evt) {
+      //인자가 없으면 기본적으로 첫번째 인자에 DOM이밴트 오브잭트가 전달된다.
+      console.log(evt)
       console.log('click')
+    },
+    test2Click(msg) {
+      console.log(msg)
+    },
+    incCount(val,evt) {
+      //전달하는 측에서 $event로 DOM 이밴트객체를 보내줄수있다.
+      console.log(evt)
+      this.count += val
     }
   }
 };
