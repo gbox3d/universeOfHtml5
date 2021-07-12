5b2862a5918ab657e29fe94ed064f2638c6d1ecb
gbox3d <gbox3d@gmail.com>
2021-07-13 01:31:19 +0900

    save store status

--- vuejs/ex03_router_1/src/store.js
+++ vuejs/ex03_router_1/src/store.js
@@ -3,19 +3,32 @@ import { createStore } from 'vuex'
 // Create a new store instance.
 export const store = createStore({
   state () {
+
+    //local storage 에 count 값 보존
+    let _count = 0
+    if(localStorage.count != undefined) {
+      _count = localStorage.count
+    } 
+    else {
+      localStorage.count = _count
+    }
+
     return {
-      count: 0
+      count: _count
     }
   },
   mutations: {
     increment (state) {
       state.count++
+      localStorage.count = state.count
     },
     decrement (state) {
       state.count--
+      localStorage.count = state.count
     },
     setvalue(state,playload) {
       state.count = playload.value
+      localStorage.count = state.count
     }
   }
 })
